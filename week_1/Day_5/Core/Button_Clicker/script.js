937595072d429b10d350de9b72e7eb40eda30e1f
OmarZalila <omarzalila525@gmail.com>
2024-02-06 01:05:59 +0100

    ajouter video

--- week_1/Day_5/Core/Button_Clicker/script.js
+++ week_1/Day_5/Core/Button_Clicker/script.js
@@ -1,5 +1,17 @@
 function logOut(element){
-    element.innerText="Logout"
+    if(element.innerText == "Login"){
+
+        element.innerText="Logout"
+    }else{
+        element.innerText="Login"
+
+    }
+}
+function addShadow(element){
+    element.classList.add("shadow");
+}
+function removeShadow(element){
+    element.classList.remove("shadow");
 }
 function hide(element){
     element.remove();
---
+++ week_1/Day_5/Practice/Nouveau dossier/video-preview/script.js
@@ -0,0 +1,8 @@
+console.log("page loaded...");
+var video = document.getElementById('dog');
+function playVideo(element) {
+    video.play();
+}
+function pauseVideo(element) {
+    video.pause();
+}
